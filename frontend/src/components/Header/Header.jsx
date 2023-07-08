b20b9423fe2bf5cba18870684821b21ed8681e72
Alisha Khatri <alisha.191502@ncit.edu.np>
2023-07-08 10:51:50 +0545

    nav completed

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,46 +1,114 @@
-import {useEffect, useRef} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { BiMenu } from 'react-icons/bi';
 import logo from '../../assets/images/logo.png';
-import {NavLink, Link} from 'react-router-dom'
+import userImg from '../../assets/images/avatar-icon.png';
 
 const navLinks = [
   {
     path: '/home',
-    display: 'Home'
+    display: 'Home',
   },
   {
     path: '/artists',
-    display: 'Find a Makeup Artists'
+    display: 'Find a Makeup Artists',
   },
   {
-    path: '/sercives',
-    display: 'Services'
+    path: '/services',
+    display: 'Services',
   },
   {
     path: '/contact',
-    display: 'Contact'
+    display: 'Contact',
   },
-]
+];
 
 const Header = () => {
+  const headerRef = useRef(null);
+  const menuRef = useRef(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleStickyHeader = () => {
+    if (
+      document.body.scrollTop > 80 ||
+      document.documentElement.scrollTop > 80
+    ) {
+      headerRef.current.classList.add('sticky__header');
+    } else {
+      headerRef.current.classList.remove('sticky__header');
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleStickyHeader);
+
+    return () => {
+      window.removeEventListener('scroll', handleStickyHeader);
+    };
+  }, []);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <header className="header flex items-center">
-      <div className='container'>
-        <div className='flex itemns-center justify-between'>
+    <header className="header flex items-center" ref={headerRef}>
+      <div className="container">
+        <div className="flex items-center justify-between">
           {/* Logo */}
           <div>
             <img src={logo} alt="" height={200} width={200} />
           </div>
 
           {/* menu */}
-          <div className='navigation items-center'>
-            <ul className='menu flex items-center gap-[2.7rem]'>
-              {
-                navLinks.map((link, index) => <li key={index}>
-                  <NavLink to={link.path} className={navClass => navClass.isActive ? 'text-primaryColor text-[16px] leading-7 font-[600]' : 'text-textColor text-[16px] leading-7 font-[500]'}>{link.display}</NavLink>
-                </li>)
-              }
-
+          <div
+            className={`navigation ${
+              isMenuOpen ? 'show__menu' : ''
+            }`}
+            ref={menuRef}
+            onClick={toggleMenu}
+          >
+            <ul className="menu flex items-center gap-[2.7rem]">
+              {navLinks.map((link, index) => (
+                <li key={index}>
+                  <NavLink
+                    to={link.path}
+                    className={navClass =>
+                      navClass.isActive
+                        ? 'text-primaryColor text-[16px] leading-7 font-[600]'
+                        : 'text-textColor text-[16px] leading-7 font-[500] hover:text-primaryColor'
+                    }
+                  >
+                    {link.display}
+                  </NavLink>
+                </li>
+              ))}
             </ul>
+          </div>
+
+          {/* ----------- nav right ---------- */}
+          <div className="flex items-center gap-4">
+            <div className="hidden">
+              <Link to="/">
+                <figure className="w-[35px] h-[35px] rounded-full cursor-pointer">
+                  <img
+                    src={userImg}
+                    className="w-full rounded-full"
+                    alt=""
+                  />
+                </figure>
+              </Link>
+            </div>
+
+            <Link to="/login">
+              <button className="bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]">
+                Login
+              </button>
+            </Link>
+
+            <span className="md:hidden" onClick={toggleMenu}>
+              <BiMenu className="w-6 h-6 cursor-pointer" />
+            </span>
           </div>
         </div>
       </div>
