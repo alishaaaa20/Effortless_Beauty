0699d571921f7cbe8f36c25e49b54979f2373e5b
Alisha Khatri <alisha.191502@ncit.edu.np>
2023-07-10 07:42:43 +0545

    ok

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <div className='flex justify-between gap-[50px] lg:gap-[130px] xl:gap-0 flex-col lg:flex-row'>
           {/*-----about image-----*/}
           <div className='relative  z-10 order-1 lg:order-2 '>
-            <img src={aboutImg} alt="" height={500} width={500} />
+            <img src={aboutImg} alt="" height={480} width={480} />
           </div>
 
           <div className='w-full lg:w-1/2 xl:w-[670px]'>
@@ -17,7 +17,7 @@ const About = () => {
               Why Us?
             </h2>
             <p className='text__para'>
-              At Effortless Beauty, you can search to find all the local Makeup Artists in your area. With us, you can view each Makeup Artist's prices, services, reviews, and more. Booking a Makeup Artist is a fantastic way to get ready for a special event. Makeup Artists can also provide recommendations for skincare products and the types of makeup that will suit your skin type and complexion. They're trained and experienced in finding the right makeup for your skin type and hair.
+              At Effortless Beauty, you can search to find all the local Makeup Artists in your area. With us, you can view each Makeup Artist's prices, services, reviews, and more. Booking a Makeup Artist is a fantastic way to get ready for a special event.  They're trained and experienced in finding the right makeup for your skin type and hair.
             </p>
             <p className='text__para mt-[30px]'>
               Take a scroll around the block to see the best suitable makeup artist on Effortless Beauty. And more often than not, when we find the perfect balance, it creates magic!
