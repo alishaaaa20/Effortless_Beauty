d935f06167511be702024d776a89a2473f77414c
Alisha Khatri <alisha.191502@ncit.edu.np>
2023-10-01 21:50:42 +0545

    auth

--- backend/models/ArtistSchema.js
+++ backend/models/ArtistSchema.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const ArtistSchema = new mongoose.Schema({
-  name: { type: String, required: true, validate: [validators.notEmpty, 'Name is empty']},
-  email: { type: String, required: true, unique: true, validate: [{ validator: validators.notEmpty, msg: 'Email is empty' }, { validator: validators.isEmail, msg: 'Invalid email' }]},
+  name: { type: String, required: true, },
+  email: { type: String, required: true, unique: true, },
   password: { type: String, required: true },
   phone: { type: Number },
   location: { type: [Number], index: { type: '2dsphere', sparse: true}},
--- backend/models/UserSchema.js
+++ backend/models/UserSchema.js
@@ -1,14 +1,10 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true, validate: [validators.notEmpty, 'Name is empty']},
-  email: { type: String, required: true, unique: true, validate: [
-    { validator: validators.notEmpty, msg: 'Email is empty' }
-  , { validator: validators.isEmail, msg: 'Invalid email' }
-  ]},
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  name: { type: String, required: true },
   phone: { type: Number },
-  location: { type: [Number], index: { type: '2dsphere', sparse: true}},
   photo: { type: String },
   role: {
     type: String,
